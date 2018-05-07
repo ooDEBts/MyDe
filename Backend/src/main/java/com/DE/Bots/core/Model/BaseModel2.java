f322fce89c9c29be5b30bcceea544cf405802d9f
ronitviper <ronit.ghosh@BRIDT-1760.BRILLIO.COM>
2018-05-07 18:30:16 +0530

    s
    Change-Id: I3120d421a09a44ebe2ef66ef24d2a728a2d248b3

--- Backend/src/main/java/com/DE/Bots/core/Model/BaseModel2.java
+++ Backend/src/main/java/com/DE/Bots/core/Model/BaseModel2.java
@@ -5,7 +5,7 @@ import org.springframework.data.annotation.Id;
 
 public class BaseModel2 {
 	
-	int myNewThingWhaTisItadscdacfr;
+	int myNewThingWhaTissItadscdacfr;
 	@Id
     private ObjectId id;
     public ObjectId getId() {
