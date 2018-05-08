5aeec489496f9d7ae309f667936ac8365bf038be
ronitviper <ronit.ghosh@BRIDT-1760.BRILLIO.COM>
2018-05-08 15:47:15 +0530

    #Comment:- Made changes as per draft
    Change-Id: Ie575cf078ace46d61dc8be35e1f30d1abaede94e

--- Backend/src/main/java/com/DE/Bots/core/Model/BidDetails.java
+++ Backend/src/main/java/com/DE/Bots/core/Model/BidDetails.java
@@ -5,7 +5,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "bidds")
 public class BidDetails extends BaseModel {
 	private String ids;
-	private String abc;
+	private String bcd;
 	private String cName;
 	private double minPrice;
 	private double sellPrice;
